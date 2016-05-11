a87799f8952508d0e1d80486e84f48b3c2848da9
0948466 <0948466@gmail.com>
2016-05-11 15:52:56 +0400

    Исправления 007

--- js/script.js
+++ js/script.js
@@ -1,29 +1,96 @@
 var linksearch = document.querySelector(".search");
 var popupsearch = document.querySelector(".modal-content-search");
-            
+var formsearch = popupsearch.querySelector(".modal-search-form");
+var search = popupsearch.querySelector(".modal-search");
+    
 linksearch.addEventListener("click", function (event) {
-  event.preventDefault();
+  event.preventDefault();      
   popupsearch.classList.add("show");
+  search.focus();    
 });
+
+formsearch.addEventListener("keydown", function (event) {
+    if (event.keyCode === 13) {
+        if (!search.value) {
+            console.log("Ошибка поиска");
+        }
+    }
+})
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popupsearch.classList.contains("show")) {
+            popupsearch.classList.remove("show");
+        }
+    }
+})
 
 var linkentrance = document.querySelector(".entrance");
 var popupentrance = document.querySelector(".modal-content-entrance");
+var formlogin = popupentrance.querySelector(".login-form");
+var loginemail = popupentrance.querySelector(".login-email");
+var formpassword = popupentrance.querySelector(".password");
+var storageloginemail = localStorage.getItem("loginemail");
 
-linkentrance.addEventListener("click", function (evententrance) {
-  evententrance.preventDefault();
-  popupentrance.classList.add("show");
+linkentrance.addEventListener("click", function (event) {
+  event.preventDefault();
+  popupentrance.classList.add("show");   
+    
+  if (storageloginemail) {
+      loginemail.value = storageloginemail;
+      formpassword.focus();
+  } else {
+      loginemail.focus();
+  }
 });
+
+formlogin.addEventListener("submit" ,function(event) {
+  if (!loginemail.value || !formpassword.value) {
+    event.preventDefault();
+    console.log("Нужно ввести логин и пароль");
+  } else {
+      localStorage.setItem("loginemail", loginemail.value);
+  }
+});
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popupentrance.classList.contains("show")) {
+            popupentrance.classList.remove("show");
+        }
+    }
+})
 
 var linkfeedback = document.querySelector(".feedback-form");
 var popupfeedback = document.querySelector(".modal-content-feedback");
 var closefeedback = document.querySelector(".modal-feedback-close");
+var formfeedback = popupfeedback.querySelector(".modal-feedback-form");
+var feedbackname = popupfeedback.querySelector(".feedback-name");
+var feedbackemail = popupfeedback.querySelector(".feedback-email");
+var feedbacktext = popupfeedback.querySelector(".feedback-text");
 
-linkfeedback.addEventListener("click", function (eventfeedback) {
-  eventfeedback.preventDefault();
+linkfeedback.addEventListener("click", function (event) {
+  event.preventDefault();
   popupfeedback.classList.add("show");
+  feedbackname.focus();    
 });
 
-closefeedback.addEventListener("click", function (eventclosefeedback) {
-    eventclosefeedback.preventDefault();
+formfeedback.addEventListener("submit", function (event) {
+    if (!feedbackname.value || !feedbackemail.value || !feedbacktext) {
+        event.preventDefault();
+        console.log("Нужно ввести данные во все поля");
+    }
+})
+
+closefeedback.addEventListener("click", function (event) {
+    event.preventDefault();
     popupfeedback.classList.remove("show");
 });
+
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (popupfeedback.classList.contains("show")) {
+            popupfeedback.classList.remove("show");
+        }
+    }
+})
